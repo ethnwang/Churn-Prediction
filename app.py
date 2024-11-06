9cd71c7ae000f23736f03b70bad990bf983fed78
Ethan Wang <ethnwang@umich.edu>
2024-11-06 19:53:06 +0900

    finished prompt engineering and created explanations, personalized emails, and graphs

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ import pandas as pd
 import pickle
 import numpy as np
 from dotenv import load_dotenv
+from openai import OpenAI
+import utils as ut
 import os
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key = os.getenv("GROQ_API_KEY")
+)
+
 
 def load_model(filename):
     # "rb" read in file as binary
@@ -67,10 +73,106 @@ def make_predictions(input_df, input_dict):
     
     avg_probability = np.mean(list(probabilities.values()))
     
-    st.markdown("### Model Probabilies")
-    for model, proba in probabilities.items():
-        st.write(f"{model} {proba}")
-    st.write(f"Average Probability: {avg_probability}")
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        fig = ut.create_gauge_chart(avg_probability)
+        st.plotly_chart(fig, use_container_width=True)
+        st.write(f"The customer has a {avg_probability:.2%} probability of churning.")
+        
+    with col2:
+        fig_probs = ut.create_model_probability_chart(probabilities)
+        st.plotly_chart(fig_probs, use_container_width=True)
+    
+    return avg_probability
+    
+    # st.markdown("### Model Probabilies")
+    # for model, proba in probabilities.items():
+    #     st.write(f"{model} {proba}")
+    # st.write(f"Average Probability: {avg_probability}")
+    
+    # return avg_probability
+    
+def explain_prediction(probability, input_dict, surname):
+    
+    prompt = f"""You are ann expert data scientist at a bank, where you specialize in interpreting and explaining prediction of machine learning models.
+    
+    Your machine learning model has predicted that a customer named {surname} has a {round(probability * 100, 1)}% probability of churning, based on the
+    information provided below.
+    
+    Here is the customer's information: {input_dict}
+    
+    Here are the machine learning model's top 10 most important features for predicting churn:
+    
+                  feature  |  importance
+    0         CreditScore  |  0.035005
+    1                 Age  |  0.109550
+    2              Tenure  |  0.030054
+    3             Balance  |  0.052786
+    4       NumOfProducts  |  0.323888
+    5           HasCrCard  |  0.031940
+    6      IsActiveMember  |  0.164146
+    7     EstimatedSalary  |  0.032655
+    8    Geography_France  |  0.046463
+    9   Geography_Germany  |  0.091373
+    10    Geography_Spain  |  0.036855
+    11      Gender_Female  |  0.045283
+    12        Gender_Male  |  0.000000
+    
+    {pd.set_option('display.max_columns', None)}
+    
+    Here are the summary statistics for churned customers: {df[df['Exited'] == 1].describe()}
+    
+    Here are the summary statistics for non-churned customers: {df[df['Exited'] == 0].describe()}
+    
+    - If the customer has over a 40% risk of churning, generate a 3 sentence explanation of why they are at risk of churning.
+    - If the customer has less than 40% risk of churning, generate a 3 sentence explanation of why they might not be at risk of churning.
+    - Your explanation should be based on the customer's information, the sumary statistics, of churned and non-churned customers, and the feature of imporatnces provided.
+    
+    
+    Don't mention the probability of churning, or the machine learning model, or say anything like "Based on the machine learning model's prediction and top 10 most
+    important features", or mention any of the customer's raw data, just explain the prediction.
+    
+    """
+    
+    print("EXPLANATION PROMPT", prompt)
+    
+    raw_response = client.chat.completions.create(
+        model = "llama-3.2-3b-preview",
+        messages=[{
+            "role" : "user",
+            "content" : prompt
+        }],
+    )
+    return raw_response.choices[0].message.content
+
+def generate_email(probability, input_dict, explanation, surname):
+    prompt = f"""You are a manager at CAP Bank. You are responsible for ensuring customers stay with the bank and are incentivezed with various offers.
+    
+    You noticed a customer named {surname} has a {round(probability * 100,1 )}% probability of churning. 
+    
+    Here is the customer's informationL {input_dict}
+    
+    Here is some explanation as to why the customer might be at risk of churning: {explanation}
+    
+    Generate an email to the customer based on their information, asking them to stay if they are at risk of chruning, or offering them incentives so that they
+    become more loyal to the bank.
+    
+    Make sure to list out a set of incentives to stay based on their information, in bullet point format. Don't ever mention the probability of churning, or
+    the machine learning model to the customer.
+    """
+    
+    raw_response = client.chat.completions.create(
+        model="llama-3.2-3b-preview",
+        messages=[{
+            "role":"user",
+            "content": prompt
+        }],
+    )
+    
+    print("\n\nEMAIL Prompt", prompt)
+    
+    return raw_response.choices[0].message.content
 
 st.title("Customer Churn Prediction")
 
@@ -169,5 +271,17 @@ if selected_customer_option:
     
     # adding machine learning model and the predicitions the model is giving us
     input_df, input_dict = prepare_input(credit_score, location, gender, age, tenure, balance, num_products, has_credit_card, is_active_member, estimated_salary)
-    make_predictions(input_df, input_dict)
+    avg_probability= make_predictions(input_df, input_dict)
+    
+    explanation = explain_prediction(avg_probability, input_dict, selected_customer['Surname'])
+    
+    st.markdown("---")
+    st.subheader("Explanation of Prediction")
+    st.markdown(explanation)
+    
+    email = generate_email(avg_probability, input_dict, explanation, selected_customer['Surname'])
+    
+    st.markdown("---")
+    st.subheader("Personalized Email")
+    st.markdown(email)
     
